a36cb8ef891286342969b6b8ba1a90c0999da43c
donghk0128 <donghk0128@gmail.com>
2026-04-04 19:38:56 +0900

    필요 데이터 폴더 및 파일  추가

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-df1 = pd.read_csv("119 신고접수 현황/신고접수_2024.csv", encoding="utf-8")
-df2 = pd.read_csv("119 신고접수 현황/신고접수_2024.csv", encoding="utf-8", dtype={"DCLR_DT": str})
+# 확인할 연도 범위
+years = range(2021, 2025)
 
-print("기본:")
-print(df1.head())
-
-print("\n문자열 강제:")
-print(df2.head())
-
-print(df1["DCLR_DT"].iloc[0], type(df1["DCLR_DT"].iloc[0]))
+for year in years:
+    file_name = f"화재사고 구조출동 현황/화재사고_{year}.csv"
+    try:
+        df = pd.read_csv(file_name)
+        rows, cols = df.shape
+        print(f"{year}.csv: {rows}개")
+    except FileNotFoundError:
+        print(f"{file_name}: 파일 없음")
+print(cols)
+    
